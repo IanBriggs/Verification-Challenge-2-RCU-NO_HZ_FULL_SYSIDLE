71f0cf7afd38f2d798a974b884af119fbd2f9b59
Ian Briggs <ianbriggs@comcast.net>
2015-04-27 03:40:21 +0000

    improved cmpxchg test

--- test/cmpxchg.c
+++ test/cmpxchg.c
@@ -6,21 +6,27 @@
 int
 main(int argc, char ** argv)
 {
-  int a;
+  int a,b;
   
   a = 0;
 
-  cmpxchg(&a, 0, 1);
+  b = cmpxchg(&a, 0, 1);
+  assert(b == 0);
+  assert(a == 1);
+  
+  b = cmpxchg(&a, 0, 12);
+  assert(b == 1);
   assert(a == 1);
 
-  cmpxchg(&a, 0, 12);
-  assert(a == 1);
-
-  cmpxchg(&a, 1, 10);
+  b = cmpxchg(&a, 1, 10);
+  assert(b == 1);
   assert(a == 10);
+
+  b = cmpxchg(&a, 10, 1);
+  assert(b == 10);
 
   /* This _should_ fail, no others */
-  int line_to_properly_fail_on = 25;
-  cmpxchg(&a, 10, 1);
+  int line_to_properly_fail_on = 30;
   assert(a == 10);
+
 }
